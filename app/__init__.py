dbcbbd499ff639be7fe634323e699e26183f5fcd
Ido Dotan <idotan@desktop.com>
2021-10-31 12:13:31 +0200

    adding file verification

--- app/__init__.py
+++ app/__init__.py
@@ -3,16 +3,17 @@ from werkzeug.utils import secure_filename
 from Job_Manager_API import Job_Manager_API
 import os
 
-UPLOAD_FOLDERS_ROOT_PATH = '/data/www/cgi-bin/fltr_backend/data/'
-USER_FILE_NAME = 'reads'
-ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+UPLOAD_FOLDERS_ROOT_PATH = '/data/www/flask/fltr_backend/data/'
+USER_FILE_NAME = 'reads.fasta'
+ALLOWED_EXTENSIONS = {'fasta', 'fastqc', 'png', 'jpg', 'jpeg', 'gif', 'pdf'}
 MAX_NUMBER_PROCESS = 3
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '168b36af-0822-4393-92c2-3e8592a48d2c'
 app.config['UPLOAD_FOLDERS_ROOT_PATH'] = UPLOAD_FOLDERS_ROOT_PATH # path to folder
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1000 * 1000 # MAX file size to upload
-manager = Job_Manager_API(MAX_NUMBER_PROCESS, UPLOAD_FOLDERS_ROOT_PATH)
+manager = Job_Manager_API(MAX_NUMBER_PROCESS, UPLOAD_FOLDERS_ROOT_PATH, USER_FILE_NAME)
 
 #@app.route("/")
 #def hello():
@@ -24,14 +25,19 @@ def allowed_file(filename):
 
 @app.route('/process_state/<process_id>')
 def process_state(process_id):
-    manager.add_process(process_id)
-    return render_template('file_download.html', process_id=process_id)
+    man_results = manager.add_process(process_id)
+    if man_results:
+        message = 'File uploaded successfully!'
+    else:
+        message = 'Verify file is of type fasta or fastqc'
 
-@app.route('/admin/running_process')
+    return render_template('file_download.html', process_id=process_id, message=message)
+
+@app.route('/admin/running')
 def running_processes():
     return render_template('runnning_processes.html', processes_ids=manager.get_running_process())
 
-@app.route('/admin/waiting_processes')
+@app.route('/admin/waiting')
 def waiting_processes():
     return render_template('waiting_processes.html', processes_ids=manager.get_waiting_process())
 
@@ -54,6 +60,6 @@ def upload_file():
             folder2save_file = os.path.join(app.config['UPLOAD_FOLDERS_ROOT_PATH'], new_process_id)
             os.mkdir(folder2save_file)
             # TODO change file name
-            file.save(os.path.join(folder2save_file, filename))
+            file.save(os.path.join(folder2save_file, USER_FILE_NAME))
             return redirect(url_for('process_state', process_id=new_process_id))
     return render_template('home.html')
---
+++ wsgi.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+
+import sys
+import site
+
+site.addsitedir('/data/www/flask/fltr_backend/venv/lib/python3.6/site-packages')
+site.addsitedir('/data/www/flask/fltr_backend/venv/lib/python3.6/site-packages/flask')
+sys.path.insert(0, '/data/www/flask/fltr_backend')
+
+from app import app as application
