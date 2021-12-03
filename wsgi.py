dce5a722dcb38f8e5ab5174bfa4316a2946a2101
Ido Dotan <idotan@desktop.com>
2021-12-03 13:42:40 +0200

    bug fixes: streaming to client, wsgi

--- wsgi.py
+++ wsgi.py
@@ -7,4 +7,4 @@ site.addsitedir('/data/www/flask/fltr_backend/venv/lib/python3.6/site-packages')
 site.addsitedir('/data/www/flask/fltr_backend/venv/lib/python3.6/site-packages/flask')
 sys.path.insert(0, '/data/www/flask/fltr_backend')
 
-from app import app as applicatio
+from app import app as application
