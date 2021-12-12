9029e9a48ccca006ea7da52542f6062f63772064
Michael Alburquerque <michaelalb@gmail.com>
2021-12-12 13:17:13 +0200

    mid fixing

--- SharedConsts.py
+++ SharedConsts.py
@@ -87,10 +87,8 @@ POST_PROCESS_COMMAND_TEMPLATE = '''
 #PBS -e {error_files_path}
 #PBS -o {output_files_path}
 
-source /groups/pupko/alburquerque/miniconda3/etc/profile.d/conda.sh
-conda activate
+source /groups/pupko/alburquerque/miniconda3/etc/profile.d/conda.csh activate
 
-set -u
 set original_unclassified_data="{path_to_original_unclassified_data}"
 set original_classified_data="{path_to_original_classified_data}"
 set input_path="{path_to_classified_results}"
@@ -100,7 +98,7 @@ set Temp_new_unclassified_seqs="Temp_new_unclassified_seqs.fasta"
 set string='{species_to_filter_on}'
 
 # filter kraken results by query name and threshold
-cat "$input_path" | awk -F "\\"*,\\"*" '{{split(var,parts,","); for (i in parts) dict[parts[i]]; if ($5 <= {classification_threshold} || !($3 in dict)) print }}' var="${{string}}" | awk -F "\\"*,\\"*" 'NR!=1 {{print $2}}' > "$output_pathTemp"
+cat "$input_path" | awk -F "," '{{split(var,parts,","); for (i in parts) dict[parts[i]]; if ($5 <= {classification_threshold} || !($3 in dict)) print }}' var="${{string}}" | awk -F "," 'NR!=1 {{print $2}}' > "$output_pathTemp"
 
 # filter original classified results
 cat "$original_classified_data" | seqkit grep -f "$output_pathTemp"  -o "$Temp_new_unclassified_seqs"
