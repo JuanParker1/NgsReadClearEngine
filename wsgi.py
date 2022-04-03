37cba5bc29288fcd2b1ace571481a4816f47a301
idotan286 <idotan286@gmail.com>
2022-04-03 16:37:12 +0300

    bug fixes, removing mutexes, fixing yeid

--- wsgi.py
+++ wsgi.py
@@ -7,4 +7,5 @@ site.addsitedir('/data/www/flask/fltr_backend/venv/lib/python3.6/site-packages')
 site.addsitedir('/data/www/flask/fltr_backend/venv/lib/python3.6/site-packages/flask')
 sys.path.insert(0, '/data/www/flask/fltr_backend')
 
+
 from app import app as application
